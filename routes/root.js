47ed78741a9c2db5d782c3aaa9f4cdfee1aa7ecc
Muky-dev <69209901+Muky-dev@users.noreply.github.com>
2021-05-25 19:28:43 -0300

    GET All Users route added

--- routes/root.js
+++ routes/root.js
@@ -3,6 +3,7 @@ import * as controller from '../controller/root.js';
 
 const router = express.Router();
 
+router.get('/api/users', controller.getAllUsers);
 router.post('/api/users', controller.postUser);
 router.post('/api/users/:_id/exercises', controller.postExercise);
 router.get('/api/users/:_id/logs', controller.getExercises);
